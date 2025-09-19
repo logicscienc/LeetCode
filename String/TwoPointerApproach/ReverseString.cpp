58f7f12015512dad6daba50142a8bd14441b70b3
logicscienc <anjuukumari001@gmail.com>
2025-09-19 23:06:52 +0530

    new Progress

---
+++ String/TwoPointerApproach/ReverseString.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int i=0;
+        int j=n-1;
+        while(i<=j){
+            swap(s[i],s[j]);
+            i++;
+            j--;
+        }
+        
+    }
+};
